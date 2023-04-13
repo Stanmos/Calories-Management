b8bf73cfddf4420393f3900d7ad83dfa26302da9
Stanislav Egorov <stasyan56@gmail.com>
2023-04-13 23:27:09 +0300

    select_user

--- src/main/java/com/nutrition/calc/web/SecurityUtil.java
+++ src/main/java/com/nutrition/calc/web/SecurityUtil.java
@@ -3,8 +3,14 @@ package com.nutrition.calc.web;
 import static com.nutrition.calc.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
+
+    private static int id = 1;
     public static int authUserId() {
-        return 1;
+        return id;
+    }
+
+    public static void setAuthUserId(int id) {
+        SecurityUtil.id = id;
     }
 
     public static int authUserCaloriesPerDay() {
