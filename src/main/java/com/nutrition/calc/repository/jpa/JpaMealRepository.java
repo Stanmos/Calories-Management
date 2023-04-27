9c50d0c2a7bd9ab2a0cd86630ba440ad3226b0f3
Stanislav Egorov <stasyan56@gmail.com>
2023-04-27 22:16:15 +0300

    add_named_query_and_transaction

--- src/main/java/com/nutrition/calc/repository/jpa/JpaMealRepository.java
+++ src/main/java/com/nutrition/calc/repository/jpa/JpaMealRepository.java
@@ -4,6 +4,7 @@ import com.nutrition.calc.model.Meal;
 import com.nutrition.calc.model.User;
 import com.nutrition.calc.repository.MealRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -11,12 +12,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaMealRepository implements MealRepository {
 
     @PersistenceContext
     private EntityManager em;
 
     @Override
+    @Transactional
     public Meal save(Meal meal, int userId) {
         meal.setUser(em.getReference(User.class, userId));
         if (meal.isNew()) {
@@ -27,8 +30,9 @@ public class JpaMealRepository implements MealRepository {
     }
 
     @Override
+    @Transactional
     public boolean delete(int id, int userId) {
-        return em.createQuery("DELETE FROM Meal m WHERE m.id=:id AND m.user.id=:userId")
+        return em.createNamedQuery(Meal.DELETE)
                 .setParameter("id", id)
                 .setParameter("userId", userId)
                 .executeUpdate() != 0;
@@ -42,17 +46,14 @@ public class JpaMealRepository implements MealRepository {
 
     @Override
     public List<Meal> getAll(int userId) {
-        return em.createQuery("SELECT m FROM Meal m WHERE m.user.id=:userId ORDER BY m.dateTime DESC", Meal.class)
+        return em.createNamedQuery(Meal.ALL_SORTED, Meal.class)
                 .setParameter("userId", userId)
                 .getResultList();
     }
 
     @Override
     public List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId) {
-        return em.createQuery("""
-                    SELECT m FROM Meal m 
-                    WHERE m.user.id=:userId AND m.dateTime >= :startDateTime AND m.dateTime < :endDateTime ORDER BY m.dateTime DESC
-                """, Meal.class)
+        return em.createNamedQuery(Meal.GET_BETWEEN, Meal.class)
                 .setParameter("userId", userId)
                 .setParameter("startDateTime", startDateTime)
                 .setParameter("endDateTime", endDateTime)
--- src/main/java/com/nutrition/calc/repository/jpa/JpaUserRepository.java
+++ src/main/java/com/nutrition/calc/repository/jpa/JpaUserRepository.java
@@ -4,29 +4,21 @@ import com.nutrition.calc.model.User;
 import com.nutrition.calc.repository.UserRepository;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import javax.transaction.Transaction;
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public class JpaUserRepository implements UserRepository {
-
-    /*
-    @Autowired
-    private SessionFactory sessionFactory;
-    private Session openSession() {
-        return sessionFactory.getCurrentSession();
-    }
-*/
 
     @PersistenceContext
     private EntityManager em;
 
     @Override
+    @Transactional
     public User save(User user) {
         if (user.isNew()) {
             em.persist(user);
@@ -42,18 +34,23 @@ public class JpaUserRepository implements UserRepository {
     }
 
     @Override
+    @Transactional
     public boolean delete(int id) {
 
 /*      User ref = em.getReference(User.class, id);
         em.remove(ref);
-*/
+
         Query query = em.createQuery("DELETE FROM User u WHERE u.id=:id");
         return query.setParameter("id", id).executeUpdate() != 0;
+ */
+        return em.createNamedQuery(User.DELETE)
+                .setParameter("id", id)
+                .executeUpdate() != 0;
     }
 
     @Override
     public User getByEmail(String email) {
-        List<User> users = em.createQuery("SELECT u FROM User u LEFT JOIN FETCH u.roles WHERE u.email=?1", User.class)
+        List<User> users = em.createNamedQuery(User.BY_EMAIL, User.class)
                 .setParameter(1, email)
                 .getResultList();
         return DataAccessUtils.singleResult(users);
@@ -61,7 +58,7 @@ public class JpaUserRepository implements UserRepository {
 
     @Override
     public List<User> getAll() {
-        return em.createQuery("SELECT u FROM User u LEFT JOIN FETCH u.roles ORDER BY u.name, u.email", User.class)
+        return em.createNamedQuery(User.ALL_SORTED, User.class)
                 .getResultList();
     }
 }
