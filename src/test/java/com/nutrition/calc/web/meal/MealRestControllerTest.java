0f8511f0386e93ee13dbd0f495f53da16699fa04
Stanislav Egorov <stasyan56@gmail.com>
2023-05-06 22:57:47 +0300

    formatters

--- src/test/java/com/nutrition/calc/web/meal/MealRestControllerTest.java
+++ src/test/java/com/nutrition/calc/web/meal/MealRestControllerTest.java
@@ -47,7 +47,9 @@ public class MealRestControllerTest extends AbstractControllerTest {
 
     @Test
     void getBetween() throws Exception {
-        perform(MockMvcRequestBuilders.get(REST_URL + "between?startDateTime=2020-01-30T07:00&endDateTime=2020-01-31T11:00:00"))
+        perform(MockMvcRequestBuilders.get(REST_URL + "filter")
+                .param("startDate", "2020-01-30").param("startTime", "07:00")
+                .param("endDate", "2020-01-31").param("endTime", "11:00"))
                 .andExpect(status().isOk())
                 .andDo(print());
     }
