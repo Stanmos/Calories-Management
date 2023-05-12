f1fd27ce60d0436b4591e36caeb092e661e4f672
Stanislav Egorov <stasyan56@gmail.com>
2023-05-12 12:04:07 +0300

    meals_binding_ajax

--- src/main/java/com/nutrition/calc/web/RootController.java
+++ src/main/java/com/nutrition/calc/web/RootController.java
@@ -1,13 +1,8 @@
 package com.nutrition.calc.web;
 
-import com.nutrition.calc.service.MealService;
-import com.nutrition.calc.service.UserService;
-import com.nutrition.calc.util.MealsUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -16,9 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 public class RootController {
     private static final Logger log = LoggerFactory.getLogger(RootController.class);
-
-    @Autowired
-    private MealService mealService;
 
     @GetMapping("/")
     public String root() {
@@ -41,10 +33,8 @@ public class RootController {
     }
 
     @GetMapping("/meals")
-    public String getMeals(Model model) {
+    public String getMeals() {
         log.info("meals");
-        model.addAttribute("meals",
-                MealsUtil.getTos(mealService.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserCaloriesPerDay()));
         return "meal";
     }
 }
