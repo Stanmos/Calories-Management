971d78abb08337d0183cea043547a95bea16384e
Stanislav Egorov <stasyan56@gmail.com>
2023-05-13 22:58:12 +0300

    secure_tag_annotation

--- src/main/java/com/nutrition/calc/web/RootController.java
+++ src/main/java/com/nutrition/calc/web/RootController.java
@@ -2,11 +2,9 @@ package com.nutrition.calc.web;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RootController {
@@ -18,6 +16,8 @@ public class RootController {
         return "redirect:meals";
     }
 
+    //    @Secured("ROLE_ADMIN")
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/users")
     public String getUsers() {
         log.info("users");
